8c6a15c0bbca7aadb4d7d331c10658445545a4d7
Adam Copp <adam.copp@gmail.com>
2017-02-04 19:27:41 +0000

    Add basic user model

--- leaveswapper.go
+++ leaveswapper.go
@@ -1,8 +1,13 @@
 package leaveswapper
 
-import "net/http"
-import "html/template"
-import "time"
+import (
+	"html/template"
+	"net/http"
+	"time"
+
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/datastore"
+)
 
 // User entry in the datastore.
 type User struct {
@@ -16,7 +21,31 @@ func init() {
 }
 
 func root(w http.ResponseWriter, r *http.Request) {
-	if err := rootTemplate.Execute(w, nil); err != nil {
+	ctx := appengine.NewContext(r)
+
+	user := User{
+		Email: "adam.copp@gmail.com",
+		Selling: []time.Time{
+			time.Date(2017, 8, 12, 0, 0, 0, 0, time.UTC),
+		},
+		Buying: []time.Time{
+			time.Date(2017, 11, 10, 0, 0, 0, 0, time.UTC),
+		},
+	}
+
+	key, err := datastore.Put(ctx, datastore.NewIncompleteKey(ctx, "User", nil), &user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var retrievedUser User
+	if err = datastore.Get(ctx, key, &retrievedUser); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := rootTemplate.Execute(w, retrievedUser); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
@@ -27,7 +56,19 @@ var rootTemplate = template.Must(template.New("root").Parse(`
     <title>Rooty</title>
   </head>
   <body>
-    <h1>Hiya, Go!</h1>
+    <h1>{{.Email}}</h1>
+		<h2>Selling</h2>
+		<ul>
+			{{range $sell := .Selling}}
+				{{$sell}}
+			{{end}}
+		</ul>
+		<h2>Buying</h2>
+		<ul>
+			{{range $buy := .Buying}}
+				{{$buy}}
+			{{end}}
+		</ul>
   </body>
 </html>
 `))
